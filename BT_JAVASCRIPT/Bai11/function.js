a54db7057e3bca5437a9d176d901a82dd7566e45
hoaithuong <huynhthihoaithuongdn@gmail.com>
2026-06-30 08:39:25 +0700

    bai11_lythuyet

---
+++ BT_JAVASCRIPT/Bai11/function.js
@@ -0,0 +1,15 @@
+// dùng Function
+function calculateArea(length, width) {
+    return length * width;
+}
+
+console.log('Area 1:', calculateArea(10, 5));
+console.log('Area 2:', calculateArea(8, 6));
+console.log('Area 3:', calculateArea(12, 4));
+
+function calculate_are(ngay, thang) {
+    return ngay + thang;
+}
+
+console.log ('amora1', calculate_are(23234,554));
+console.log ('amora2', calculate_are(43,33333));
